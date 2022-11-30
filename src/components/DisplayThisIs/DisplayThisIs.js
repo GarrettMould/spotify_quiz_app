a365ec4bbf71c81f24b16dbe6bbd01f77fa19bfe
Garrett Mould <garrettmould@gmail.com>
2022-11-30 08:45:27 +0700

    Added UI display for quiz

--- src/components/DisplayThisIs/DisplayThisIs.js
+++ src/components/DisplayThisIs/DisplayThisIs.js
@@ -1,19 +1,48 @@
 import React from 'react'
+import classes from './DisplayThisIs.module.css'
 
 const DisplayThisIs = (props) => {
 
-    function Playlist(name, img, allSongs, selectedSongs) {
-        this.name = name;
-        this.img = img; 
-        this.allSongs = allSongs; 
-        this.selectedSongs = selectedSongs;
-    }
+  var selectedSongs = props.selectedThisIsSongs;
+     // Map over selectedSongs and create a UI for the test questions
+  const mappedSongs = selectedSongs.map((song) => { 
+    var correctAnswer = song.name
 
+    const mappedAnswerOptions = song.answerOptions.map( (song, i) => { 
+        return (
+            <>
+            <div className={classes.answerContainer}>
+            
+            <button
+              className={classes.btnAnswerOption}
+              type="radio"
+              name={correctAnswer}
+              value={song.name === correctAnswer ? true : false}
+              
+            ><div className={classes.imgContainer}><img src={song.img.url} alt="album" className={classes.img}></img></div><div className={classes.answerText}>{song.name}</div></button>
+            </div>
+            </>
+            
+          
+            
+        )})
+
+    return (
+        <>
+        <audio controls="controls">
+        <source src={song.uri} type="audio/mpeg"/>
+        </audio>
+        {mappedAnswerOptions}
+        
+        </>
+    )
+})
 
   return (
     <>
     <div><img src={props.thisIsImage} alt="playlist"></img></div>
     <div>{props.thisIsName}</div>
+    {mappedSongs}
     </>
   )
 }
