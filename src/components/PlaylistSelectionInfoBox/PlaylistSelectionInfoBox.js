934140f6c06acec8d3c21ceb40aa597c5cee011f
Garrett Mould <garrettmould@gmail.com>
2023-01-11 09:11:22 +0700

    color updates

--- src/components/PlaylistSelectionInfoBox/PlaylistSelectionInfoBox.js
+++ src/components/PlaylistSelectionInfoBox/PlaylistSelectionInfoBox.js
@@ -7,7 +7,7 @@ const PlaylistSelectionInfoBox = () => {
       <div className={classes.contentWrapper}>
         <div className={classes.categoryHeadline}>Hip-Hop & Rap</div>
         <div className={classes.mainHeadline}>Hip-Hop's Biggest Hitmakers</div>
-        <div className={classes.description}>Test your knowledge of your favorite artists in the world of hip-hop. Are you truly the stan that you think you are?</div>
+        <div className={classes.description}>Test your knowledge of your favorite artists in the world of hip-hop.</div>
         <button className={classes.btn}>Browse All</button>
       </div>
     </div>
